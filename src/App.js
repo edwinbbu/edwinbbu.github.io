66f4dfd8704dea4543d9b0eeb6d00d96678a3e8f
Edwin Babu <edwinbbu@gmail.com>
2020-09-05 13:40:36 +0530

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <header className="App-header bg-red-600">
+        <p className="bg-gray-600">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
